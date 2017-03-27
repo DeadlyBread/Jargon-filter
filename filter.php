046c492abd6362bc57f9f23ed38e0212c9b8fd28
DeadlyBread <paskohli@aol.com>
2017-03-28 00:06:11 +0100

    3

--- filter.php
+++ filter.php
@@ -4,25 +4,28 @@
     if (isset($_POST["text"])) {
         $text = $_POST["text"];
 
-        //Sanitise text
-        $text = mysqli_real_escape_string($dbc, $text);
-        $text = strip_tags($text);
-
         $text_lower = strtolower($text);
-        $text_lower = str_replace("-", " ", $text_lower);
+        $text_lower = str_replace("", " ", $text_lower);
+
+        //Sanitise text
+        $text_lower = mysqli_real_escape_string($dbc, $text_lower);
+        $text_lower = strip_tags($text_lower);
 
         $q = mysqli_query($dbc, "select * from terms where '" . $text_lower . "' LIKE CONCAT('%', term,'%');"); //Query database to return jargon terms that text contains
+        
         if ($q) {
+            $count = 0;
             $count_lower = 0;
             $count_capital = 0;
 
             while ($r = mysqli_fetch_array($q, MYSQLI_NUM)) {
                 $text = preg_replace("/[A-Z](?i)(" . substr_replace($r[0], "", 0, 1) . "|" . substr_replace(str_replace(" ", "-", $r[0]), "", 0, 1) . ")\w*/", "Blah", $text, -1, $count_capital);
                 $text = preg_replace("/(" . $r[0] . "|" . str_replace(" ", "-", $r[0]) . ")\w*/i", "blah", $text, -1, $count_lower);
+
+                $count += $count_lower + $count_capital;
             }
 
-            $count = $count_lower + $count_capital;
-            echo $text . " (jargon count: " . $count . ") [jargon score: " . score($count, $text) . "/100] << 0 - jargon free | 25 - some jargon | 50 - getting dangerous | 75 - guff city >>";
+            echo $text . "\n\n(jargon count: " . $count . ") [jargon score: " . score($count, $text_lower) . "/100] << 0 - jargon free | 25 - some jargon | 50 - getting dangerous | 75 - guff city >>";
         }
     }
 
@@ -32,6 +35,6 @@
         if ($s > 100) $s = 100;
         else if ($s < 0) $s = 0;
 
-        return $s;
+        return round($s, 0);
     }
 ?>
